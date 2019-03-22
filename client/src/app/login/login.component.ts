31c8c378178f22ebd676d9d3e82833fd1278b96e
Daniel_S <dfsimeon@gmail.com>
2019-03-22 17:06:41 -0400

    some progress on login functions

--- client/src/app/login/login.component.ts
+++ client/src/app/login/login.component.ts
@@ -9,11 +9,35 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 export class LoginComponent implements OnInit { //default
 
-  constructor() {//default
+  angForm: FormGroup;
 
+  //default method
+  constructor(private formBuilder: FormBuilder ) {
+      this.createForm();
    }
+
+   createForm(){
+     this.angForm = this.formBuilder.group({ firstname: ['', Validators.compose([ Validators.required,  this.validateName ])], });
+  }
+
+  onLoginSubmit(){
+    const user = {
+      firstname: this.angForm.get('firstname').value,
+      lastname: this.angForm.get('lastname').value,
+      email: this.angForm.get('email').value,
+      password: this.angForm.get('password').value
+};
+  }
+
+
+
+
+
+
+
+
+
 
   ngOnInit() {//default
   }
-
 }
